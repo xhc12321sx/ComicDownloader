8c0d7f9ca062d850ff7a09f172126fab5532a459
unknown <xhc12321sx@126.com>
2019-12-08 18:31:05 -0800

    loop

--- from_mangabz.py
+++ from_mangabz.py
@@ -7,26 +7,31 @@ if __name__ == "__main__":
     if not os.path.exists("./download"):
         os.mkdir("./download")
 
-    # a = spider(headless=False, dtLoadPicture=False)
+    
     a = spider(headless=False)
-    # url = input("Url:")
-    url = "http://www.mangabz.com/38bz/"
 
-    if re.search(r"mangabz.com/m", url):
-        a.get_url(url)
-    else:
-        a.browser.get(url)
-        a.browser.find_element_by_class_name("detail-list-form-more").click()
-        urls = a.browser.find_elements_by_class_name("detail-list-form-item")
-        print("Totally {0} comics".format(len(urls)))
-        for i in range(len(urls)):
-            urls[i]= urls[i].get_attribute("href")
-        urls.reverse()
-        i = 1
-        for url in urls:
-            print("No. {0}:".format(i))
+    while 1:
+        url = input("Url (exit to exit):")
+        # url = "http://www.mangabz.com/38bz/"
+
+        if url == "exit":
+            break
+
+        if re.search(r"mangabz.com/m", url):
             a.get_url(url)
-            i += 1
-        print("Done")
-        pass
+        else:
+            a.browser.get(url)
+            a.browser.find_element_by_class_name("detail-list-form-more").click()
+            urls = a.browser.find_elements_by_class_name("detail-list-form-item")
+            print("Totally {0} comics".format(len(urls)))
+            for i in range(len(urls)):
+                urls[i]= urls[i].get_attribute("href")
+            urls.reverse()
+            i = 1
+            for url in urls:
+                print("No. {0}:".format(i))
+                a.get_url(url)
+                i += 1
+            print("Done")
+            pass
     
